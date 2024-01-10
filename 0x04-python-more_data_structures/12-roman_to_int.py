ce50789317b33050aea207659cb540bbedfb0323
deezyfg <mensahpeter421@gmail.com>
2024-01-10 03:14:13 -0800

    Added all tasks.

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def roman_to_int(roman_string):
+    if not roman_string or type(roman_string) != str:
+        return 0
+    digits = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+    for roman in reversed(roman_string):
+        decimal = digits[roman]
+        total += decimal if total < decimal * 5 else -decimal
+    return result
