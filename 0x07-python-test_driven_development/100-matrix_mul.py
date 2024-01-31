e6741d498ede39cb85d4fcdae4583597c0f4cb84
deezyfg <mensahpeter421@gmail.com>
2024-01-30 22:12:58 -0800

    Added all tasks.

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -51,14 +51,14 @@ def matrix_mul(m_a, m_b):
 
     for elems in m_a:
         if length != 0 and length != len(elems):
-            raise TypeError("each row of m_a must should be of the same size")
+            raise TypeError("each row of m_a must be of the same size")
         length = len(elems)
 
     length = 0
 
     for elems in m_b:
         if length != 0 and length != len(elems):
-            raise TypeError("each row of m_b must should be of the same size")
+            raise TypeError("each row of m_b must be of the same size")
         length = len(elems)
 
     if len(m_a[0]) != len(m_b):
